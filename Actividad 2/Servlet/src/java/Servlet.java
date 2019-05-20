27bc6f501a46128ecad15ae22b57ec702f59f98e
alexgr2699 <47648426+alexgr2699@users.noreply.github.com>
2019-05-19 23:10:35 -0500

    Add files via upload

---
+++ Actividad 2/Servlet/src/java/Servlet.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        String Nombre = request.getParameter("Nom");
+        String ApellidoP = request.getParameter("Ap");
+        String ApellidoM = request.getParameter("Am");
+        String CorreoE = request.getParameter("Ce");
+        String Contrasenia = request.getParameter("Co");
+        /* TODO output your page here. You may use folowing samble code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Tu nombre es: " + Nombre + "</h1>");
+            out.println("<h1>Tu Apellido P. es: " + ApellidoP + "</h1>");
+            out.println("<h1>Tu Apellido M. es: " + ApellidoM + "</h1>");
+            out.println("<h1>Tu Correo es: " + CorreoE + "</h1>");
+            out.println("<h1>Tu Contrasenia es: " + Contrasenia + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
